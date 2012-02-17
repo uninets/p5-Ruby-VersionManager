4bdf101d202cef4d1063f85dee55156cbaf39dfc
Mugen Kenichi <mugen.kenichi@uninets.eu>
2012-02-17 16:07:05 +0100

    fetch and unpack source

--- lib/Ruby/VersionManager.pm
+++ lib/Ruby/VersionManager.pm
@@ -10,15 +10,18 @@ use Moo;
 use YAML;
 use LWP::UserAgent;
 use HTTP::Request;
+use LWP::Simple;
 
 has rootdir => ( is => 'rw' );
 has ruby_version => ( is => 'rw' );
-has rubygems_version => (is => 'rw' );
+has rubygems_version => ( is => 'rw' );
 has available_rubies => ( is => 'rw' );
+has agent_string => ( is => 'rw' );
 
 sub BUILD {
     my ($self) = @_;
 
+    $self->agent_string('Ruby::VersionManager/0.01');
     $self->_make_base or die;
     $self->_check_db or die;
 }
@@ -30,12 +33,12 @@ sub _make_base {
 
     if (not -d $self->rootdir){
         say "root directory for installation not found.\nbootstraping to " . $self->rootdir;
-        mkdir $self->rootdir or return 0;
-        mkdir $self->rootdir . '/bin' or return 0;
-        mkdir $self->rootdir . '/source' or return 0;
-        mkdir $self->rootdir . '/var' or return 0;
-        mkdir $self->rootdir . '/gemsets' or return 0;
-        mkdir $self->rootdir . '/rubies' or return 0;
+        mkdir $self->rootdir;
+        mkdir $self->rootdir . '/bin';
+        mkdir $self->rootdir . '/source';
+        mkdir $self->rootdir . '/var';
+        mkdir $self->rootdir . '/gemsets';
+        mkdir $self->rootdir . '/rubies';
     }
 
     return 1;
@@ -65,7 +68,7 @@ sub update_db {
         my $req = HTTP::Request->new(GET => $ruby_ftp);
 
         my $ua = LWP::UserAgent->new;
-        $ua->agent("Ruby::VersionManager/0.01");
+        $ua->agent($self->agent_string);
 
         my $res = $ua->request($req);
 
@@ -112,7 +115,7 @@ sub _guess_version {
         }
     }
 
-    my $guess = ((sort @rubies)[-1]);
+    my $guess = ((sort { $a->[1] cmp $b->[1] } @rubies)[-1]);
 
     if (not $guess){
         say "No matching version found. Valid versions:";
@@ -128,17 +131,48 @@ sub install {
     my ($self) = @_;
 
     my $version = $self->_guess_version;
-    $self->_fetch_ruby($version);
+    $self->ruby_version($version);
+    $self->_fetch_ruby;
+    $self->_unpack;
+    $self->_make;
 
 }
 
+sub _unpack {
+    my ($self) = @_;
+
+    system 'tar xf ' . $self->rootdir . '/source/' . $self->ruby_version->[1]
+            . ' -C  '
+            . $self->rootdir . '/source/';
+
+    return 1;
+}
+
+sub _make {
+    my ($self) = @_;
+
+    return 1;
+}
+
 sub _fetch_ruby {
-    my ($self, $version) = @_;
+    my ($self) = @_;
 
     my $url = 'ftp://ftp.ruby-lang.org/pub/ruby/'
-              . $version->[0]
+              . $self->ruby_version->[0]
               . '/'
-              . $version->[1];
-    say $url;
+              . $self->ruby_version->[1];
+
+    my $file = $self->rootdir . '/source/' . $self->ruby_version->[1];
+
+    if ( -f $file ){
+        return 1
+    }
+
+    my $result = LWP::Simple::getstore($url, $file);
+
+    die if $result != 200;
+
+    return 1;
+
 }
 
